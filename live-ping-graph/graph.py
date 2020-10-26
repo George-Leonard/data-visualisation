e98a50aad03614d9ca65add4b73a1da3dba75359
George <38766881+George-Leonard@users.noreply.github.com>
2020-10-26 14:24:45 +0000

    Update graph.py

--- live-ping-graph/graph.py
+++ live-ping-graph/graph.py
@@ -1,9 +1,12 @@
 
+import mplcursors
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 from matplotlib import style
+import matplotlib.dates as mdates
 
 style.use("ggplot")
+
 
 fig = plt.figure()
 ax1 = fig.add_subplot(1,1,1)
@@ -16,19 +19,27 @@ def animate(i):
     
     data = open("PINGINFO.txt", "r").read()
     lines = data.split("\n")
+    
     for line in lines:
         if len(line) > 1:
             line = line.split(",")
+            line[0] = mdates.datestr2num(line[0])
             xs.append(line[0])
+            
             ys.append(float(line[1]))
 
+            ax1.clear()
+            plt.xlabel('Timeline (H:M:S)')
+            plt.ylabel('Ping (ms)')
+            plt.xticks(rotation=90)
 
-    ax1.clear()
-    plt.xlabel('Time')
-    plt.ylabel('Ping')
-    plt.xticks(rotation=70)
-    ax1.plot(xs,ys,'-o')
+
+
+    lines = ax1.plot(xs,ys)
+    f = mdates.DateFormatter('%H:%M:%S')
+    ax1.xaxis.set_major_formatter(f)
+    mplcursors.cursor(lines,hover=True)
+    plt.gcf().autofmt_xdate()
     
-
 ani = animation.FuncAnimation(fig, animate, interval=1000)
 plt.show()
