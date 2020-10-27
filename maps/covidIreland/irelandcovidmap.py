57d5fed2d325cbfd82ed71ab96cffbd2a2bfb75c
George <38766881+George-Leonard@users.noreply.github.com>
2020-10-27 17:44:27 +0000

    Update irelandcovidmap.py

--- maps/covidIreland/irelandcovidmap.py
+++ maps/covidIreland/irelandcovidmap.py
@@ -2,36 +2,41 @@ import geopandas as gpd
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib import style
+import numpy as np
+import mplcursors
 
 style.use("dark_background")
-
 irelandcovid = gpd.read_file("Covid19_LEACases_Mapped-shp/Covid19_LEACases_Mapped.shp")
-
 cases = pd.read_csv("Covid19_LEACases_Mapped.csv")
 
-#pd.set_option('display.max_columns', None)
-#print(irelandcovid.head())
-
+pd.set_option('display.max_columns', None)
 cases = irelandcovid['C19_P14_T']
+populations = irelandcovid['Pop2016']
 
 for i in range(0,len(cases)):
     try:
         cases[i] = float(cases[i])
+        #print("POPULATION:",irelandcovid["Pop2016"][i],irelandcovid['ENGLISH'][i],":",cases[i])
+        #EVIDENT CORRELATION BETWEEN POPULATION AND CASES
     except:
-        cases[i] = 3.0
+        cases[i] = 1
 
-fig = plt.figure()
-ax1 = fig.add_subplot(111)
-fig.suptitle("Covid19 cases around ireland")
+figcorr = plt.figure()
+figcorr.suptitle("Correlation between population of a location and recorded Covid19 cases")
+axcorr = figcorr.add_subplot(111)
 
+plt.scatter(populations,cases)
 
-irelandcovid.plot(ax=ax1,column=cases,cmap='Blues')
-cbar = plt.cm.ScalarMappable(cmap='Blues',
-                           norm=plt.Normalize(
-                               vmin=min(cases), vmax=max(cases)))
+mplcursors.cursor(axcorr).connect(
+    "add", lambda sel: sel.annotation.set_text(
+        irelandcovid["ENGLISH"][sel.target.index]))
+figmap = plt.figure()
+axmap = figmap.add_subplot(111)
+figmap.suptitle("Covid19 cases around ireland")
+irelandcovid.plot(ax=axmap,column=cases,cmap='Blues')
 
-ax1.axis('off')
-
-cbar = fig.colorbar(cbar)
+cbar = plt.cm.ScalarMappable(cmap='Blues',norm=plt.Normalize(vmin=min(cases), vmax=max(cases)))
+axmap.axis('off')
+cbar = figmap.colorbar(cbar)
 
 plt.show()
